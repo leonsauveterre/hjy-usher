f8f1039245b2ce072785297276d3ec92c709c99a
Leon <zhaoliang03@huice.com>
2024-12-10 10:32:33 +0800

    Seems like the last modification for `bills-logs`within this month!

--- src/app/sidebar-menu/bills/logs/bills-logs.component.ts
+++ src/app/sidebar-menu/bills/logs/bills-logs.component.ts
@@ -7,6 +7,13 @@ interface SimpleLog {
   description: string;
 }
 
+interface ComplexLog {
+  identifier: string;
+  scene: string;
+  platformAlias: string;
+  category: string
+}
+
 @Component({
   selector: 'app-logs',
   imports: [
@@ -17,10 +24,13 @@ interface SimpleLog {
   styleUrl: './bills-logs.component.css'
 })
 export class BillsLogsComponent {
-  searchTerm: string = '';
+  searchTerm: string = '';  // opTypes
+  searchTerm2: string = '';  // opSummaries
+  searchTerm3: string = '';  // authTypes
+  searchTerm4: string = '';  // authSummaries
   selectedCategory: string = '';
 
-  readonly opTypes: Array<{ identifier: string; scene: string; platformAlias: string; category: string }> = [
+  readonly opTypes: Array<ComplexLog> = [
     { identifier: 'c1', scene: '支付宝账单', platformAlias: 'alipay_money', category: '资金账单' },
     { identifier: 'd1', scene: '京东商城货款账单', platformAlias: 'jd_money', category: '资金账单' },
     { identifier: 'd2', scene: '京东钱包', platformAlias: 'jd_money', category: '资金账单' },
@@ -64,26 +74,107 @@ export class BillsLogsComponent {
     { identifier: 'k2', scene: '抖音巨量千川-全域推广', platformAlias: 'doudian_money', category: '直播账单' }
   ];
 
-  readonly opSummaries: Array<SimpleLog> = [];
+  readonly opSummaries: Array<SimpleLog> = [
+    { identifier: 'b1', description: '账单导入' },
+    { identifier: 'b2', description: '账单已审核' },
+    { identifier: 'b3', description: '账单已完成' },
+    { identifier: 'b4', description: '账单删除中' },
+    { identifier: 'b5', description: '账单已删除' },
+    { identifier: 'b6', description: '账单分摊中' },
+    { identifier: 'c1', description: '手动执行账单分类' },
+    { identifier: 'c2', description: '手动刷新账单分类' },
+    { identifier: 'c3', description: '执行账单分类完成' },
+    { identifier: 'c4', description: '刷新账单分类完成' },
+    { identifier: 'd1', description: '系统拉取账单开始' },
+    { identifier: 'd2', description: '手动拉取账单开始' },
+    { identifier: 'd3', description: '系统拉取账单完成' },
+    { identifier: 'd4', description: '手动拉取账单完成' },
+    { identifier: 'd5', description: '系统拉取账单失败' },
+    { identifier: 'e1', description: '手动分配店铺' },
+    { identifier: 'e2', description: '手动分类费用项' },
+    { identifier: 'e3', description: '手动分类费用项-分类开始' },
+    { identifier: 'e4', description: '手动分类费用项-分摊结束' },
+    { identifier: 'e5', description: '费用项分摊开始' },
+    { identifier: 'e6', description: '费用项分摊完成' },
+    { identifier: 'h0', description: '手动调整异常任务状态' }
+  ];
 
-  readonly authTypes: Array<SimpleLog> = [];
+  readonly authTypes: Array<SimpleLog> = [
+    { identifier: '1', description: '店铺操作' },
+    { identifier: '2', description: '资金账单' },
+    { identifier: '3', description: '营销账单' },
+    { identifier: '4', description: '其他账单' },
+  ];
 
-  readonly authSummaries: Array<SimpleLog> = [];
+  readonly authSummaries: Array<SimpleLog> = [
+    { identifier: 's1', description: '启用店铺' },
+    { identifier: 's2', description: '停用店铺' },
+    { identifier: 's3', description: '编辑资金账号ID' },
+    { identifier: 's4', description: '编辑营销账号ID' },
+    { identifier: 'b1', description: '资金账单授权成功' },
+    { identifier: 'b2', description: '编辑资金账号ID，授权失效' },
+    { identifier: 'b3', description: '资金账单授权失败' },
+    { identifier: 'b4', description: '资金账单取消授权' },
+    { identifier: 'b5', description: '系统拉取账单失败：资金账单授权失效' },
+    { identifier: 'b6', description: '手动拉取账单失败：资金账单授权失效' },
+    { identifier: 'b7', description: '授权校验：资金账单授权失效' },
+    { identifier: 'm1', description: '营销账单授权成功' },
+    { identifier: 'm2', description: '编辑营销账号ID，授权失效' },
+    { identifier: 'm3', description: '营销账单授权失败' },
+    { identifier: 'm4', description: '营销账单取消授权' },
+    { identifier: 'm5', description: '系统拉取账单失败：营销账单授权失效' },
+    { identifier: 'm6', description: '手动拉取账单失败：营销账单授权失效' },
+    { identifier: 'm7', description: '授权校验：营销账单授权失效' },
+    { identifier: 'u1', description: '系统拉取账单失败：其他账单授权失效' },
+    { identifier: 'u2', description: '手动拉取账单失败：其他账单授权失效' }
+  ];
+
+  speedDialTo(target: 'op-type' | 'op-summary' | 'auth-type' | 'auth-summary'): void {
+    const dest = document.getElementById('scroll-to-' + target);
+    if (dest) {
+      dest.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  private filterSimpleLogs(source: SimpleLog[], searchTerm: string): SimpleLog[] {
+    return source.filter(e => {
+      return searchTerm
+        ? (
+          e.identifier.toLowerCase().includes(searchTerm.toLowerCase())
+          || e.description.toLowerCase().includes(searchTerm.toLowerCase())
+        ) : true;
+    });
+  }
 
   // op - types
   opCategories: string[] = Array.from(new Set(this.opTypes.map(log => log.category)));
 
-  get filterOpTypes() {
+  get filterOpTypes(): ComplexLog[] {
     return this.opTypes.filter(e => {
       const matchesSearch = this.searchTerm
         ? (
-          e.identifier.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-          e.scene.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-          e.platformAlias.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-          e.category.toLowerCase().includes(this.searchTerm.toLowerCase())
+          e.identifier.toLowerCase().includes(this.searchTerm.toLowerCase())
+            || e.scene.toLowerCase().includes(this.searchTerm.toLowerCase())
+            || e.platformAlias.toLowerCase().includes(this.searchTerm.toLowerCase())
+            || e.category.toLowerCase().includes(this.searchTerm.toLowerCase())
         ) : true;
       const matchesCategory = this.selectedCategory ? e.category === this.selectedCategory : true;
       return matchesSearch && matchesCategory;
     });
+  }
+
+  // op - summaries
+  get filterOpSummaries(): SimpleLog[] {
+    return this.filterSimpleLogs(this.opSummaries, this.searchTerm2);
+  }
+
+  // auth - types
+  get filterAuthTypes(): SimpleLog[] {
+    return this.filterSimpleLogs(this.authTypes, this.searchTerm3);
+  }
+
+  // auth - summaries
+  get filterAuthSummaries(): SimpleLog[] {
+    return this.filterSimpleLogs(this.authSummaries, this.searchTerm4);
   }
 }
