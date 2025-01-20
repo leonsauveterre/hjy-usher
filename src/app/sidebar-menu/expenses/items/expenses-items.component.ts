643638909fbbb14ff2230bb17b5ee61476959f10
Leon Zhao <zhaoliang03@huice.com>
2025-01-20 18:43:28 +0800

    A brief version of `expenses-items` because we value our time.

--- src/app/sidebar-menu/expenses/items/expenses-items.component.ts
+++ src/app/sidebar-menu/expenses/items/expenses-items.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { CodeDisplayComponent } from '../../../code-display/code-display.component';
 
 @Component({
   selector: 'app-items',
-  imports: [],
+  imports: [
+    CodeDisplayComponent
+  ],
   templateUrl: './expenses-items.component.html',
   styleUrl: './expenses-items.component.css'
 })
 export class ExpensesItemsComponent {
+  protected presetStatements: readonly string[] = [
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'is_show_refresh_button';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'is_open_time';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'mark_standard_limit';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'mark_account_limit';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'mark_total_standard_limit';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'mark_total_account_limit';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'tolerable_abnormal_decimal';`,
+
+    `SELECT *
+FROM bill_enum
+WHERE enum_name = 'custom_marketing_import_trustworthy';`,
+  ];
 
 }
